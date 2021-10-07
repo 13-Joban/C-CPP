6ea8002a01752b3876963c68fe0e9f5b3b9245f9
Jobanpreet <74794315+13-Joban@users.noreply.github.com>
2021-10-07 18:17:38 +0530

    Create Jobanpreet Singh 2004603_ PR8.cpp

---
+++ Jobanpreet Singh 2004603_ PR8.cpp
@@ -0,0 +1,26 @@
+// we will see how to calculate sum of array using recursion
+
+#include<iostream>
+using namespace std;
+
+int sum(int input[], int n) {
+  if(n==0)
+      return 0;
+    
+   int total= input[0] + sum (input+1,n-1);
+    return total;
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    cout << sum(input, n) << endl;
+}
