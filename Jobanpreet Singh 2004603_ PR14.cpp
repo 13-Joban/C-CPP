250448138304c2853ec3b6328ba73d3ea2141a5a
Jobanpreet <74794315+13-Joban@users.noreply.github.com>
2021-10-09 16:42:37 +0530

    Create Jobanpreet Singh 2004603_ PR14.cpp

---
+++ Jobanpreet Singh 2004603_ PR14.cpp
@@ -0,0 +1,23 @@
+// c++ program to find area of triangle 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b, h, area;
+    cout<<"Enter Base length of Triangle: ";
+    cin>>b;
+    cout<<"Enter Height length of Triangle: ";
+    cin>>h;
+    area = 0.5*b*h;
+    cout<<"\nArea = "<<area;
+    cout<<endl;
+    return 0;
+}
+
+
+Output : 
+Enter Base length of Triangle: 5
+Enter Height length of Triangle: 10
+Area = 25
+  
